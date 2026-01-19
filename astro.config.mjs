b039e1105d317eff54b87ada93ca906db4cb6731
seunghoonKang <tmdgns4321@gmail.com>
2026-01-19 21:43:23 +0900

    chore: 프로젝트 설정 및 의존성 업데이트
    - Tailwind CSS, MDX, Notion API 등 필요한 패키지 추가
    - Astro 설정에 Tailwind, MDX 통합 및 마크다운 설정 추가
    - Prettier 설정 파일 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
+import remarkToc from "remark-toc";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    // eslint-disable-next-line
+    // @ts-ignore
+    plugins: [tailwindcss()],
+  },
+ integrations: [ mdx()],
+  markdown: {
+     remarkPlugins: [
+      [remarkToc, { 
+        heading: '목차',
+        maxDepth: 3,
+        tight: true
+      }]
+    ],
+    shikiConfig: { theme: "github-dark" },
+  },
+});
